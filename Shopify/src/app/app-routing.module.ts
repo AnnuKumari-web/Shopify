a1805282c3992e48e2f38106b24e64ea58e1504c
Annu Kumari <akumari9@slb.com>
2022-12-16 19:33:26 +0530

    added product

--- Shopify/src/app/app-routing.module.ts
+++ Shopify/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './navbar/home/home.component';
+import { ProductListComponent } from './Product/product-list/product-list.component'
+import { ProductDetailComponent } from './Product/product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductListComponent},
+  { path: 'product-detail', component: ProductDetailComponent},
+  { path: 'more', component: ProductDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Shopify/src/app/app.module.ts
+++ Shopify/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './Product/product-list/product-list.component';
+import { WelcomeComponent } from './navbar/welcome/welcome.component';
+import { HomeComponent } from './navbar/home/home.component';
+import { HeaderComponent } from './navbar/header/header.component';
+import { ProductDetailComponent } from './Product/product-detail/product-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductCardComponent } from './Product/product-card/product-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     WelcomeComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    ProductDetailComponent,
+    NavbarComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
